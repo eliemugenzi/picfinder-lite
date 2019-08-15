724e061c93df7144df78c11a327a85f405e715ce
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-08-15 13:56:40 +0200

    feature(project): Display images after a successful search

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import AppBAr from 'material-ui/AppBar';
+import { Link } from 'react-router-dom';
 
-const NavBar = () => <AppBAr title="PicFinder Lite" />;
+const NavBar = () => <div>
+  <nav className="teal">
+    <div className="nav-wrapper container">
+      <Link to='/' href="#" className="brand-logo">PicFinder Lite</Link>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><Link to="/search">Search</Link></li>
+        <li><Link to="/about">About the app</Link></li>
+        <li><Link to="/contact">Contact the developer</Link></li>
+      </ul>
+    </div>
+  </nav>
+</div>;
 
 export default NavBar;
--- src/redux/actions/fetchImages.js
+++ src/redux/actions/fetchImages.js
@@ -5,6 +5,7 @@ export const fetchImages= (searchText, amount) => async dispatch => {
   const REQUEST_URL = `${process.env.REACT_APP_API_URL}/?key=${process.env.API_KEY}&q=${searchText}&image_type=photo&per_page=${amount}&safesearch=true`;
   try {
     const { data: response } = await axios.get(REQUEST_URL);
+    console.log(response);
     dispatch({
       type: IMAGES_FETCHED,
       payload:response.hits
