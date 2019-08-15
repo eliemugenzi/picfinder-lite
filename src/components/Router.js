91667f5fe56585c86101a8433a1cd7f3a00daec9
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-08-15 17:01:15 +0200

    feature(project): Add About and Contact pages

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './Navbar';
 import Home from './pages/Home';
 import Search from './pages/Search';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const MainRouter = () => (
   <Router>
@@ -10,6 +12,8 @@ const MainRouter = () => (
     <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/search' component={Search} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/contact' component={Contact}/>
     </Switch>
   </Router>
 );
