e5fe3abc2abd895717679b06807723af13cfbc4a
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-08-15 17:01:15 +0200

    feature(project): Add About and Contact pages

--- src/redux/actions/fetchImages.js
+++ src/redux/actions/fetchImages.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { IMAGES_FETCHED } from './types';
 
-export const fetchImages= (searchText, amount) => async dispatch => {
-  const REQUEST_URL = `${process.env.REACT_APP_API_URL}/?key=${process.env.API_KEY}&q=${searchText}&image_type=photo&per_page=${amount}&safesearch=true`;
+export const fetchImages = (searchText, amount) => async dispatch => {
+  const REQUEST_URL = `${process.env.REACT_APP_API_URL}/?key=${process.env.REACT_APP_API_KEY}&q=${searchText}&image_type=photo&per_page=${amount}&safesearch=true`;
+  console.log(REQUEST_URL);
   try {
     const { data: response } = await axios.get(REQUEST_URL);
     console.log(response);
